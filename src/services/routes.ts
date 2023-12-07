affa0b8f579e12989ce8be95404b0303b5262a5d
Nikita <world.devband777@gmail.com>
2023-12-07 15:58:17 +0300

    Initial commit

---
+++ src/services/routes.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import * as controller from './controller';
+
+export const router = express.Router();
+
+router.route('/presale-info').get(controller.getPresaleInfo);
+router.route('/purchase-info').get(controller.getPurchaseInfo);
+router.route('/mypurchase-info/:walletaddress').get(controller.getMyTokenSaleInfo);
+
+router.route('/generate-signature').post(controller.generateSignature);
+router.route('/request-sol').post(controller.requestSOL);
+router.route('/process-evm').post(controller.processEVM);
+router.route('/process-sol').post(controller.processSOL);
